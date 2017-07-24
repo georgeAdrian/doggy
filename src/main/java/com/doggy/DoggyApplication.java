2341ebbf843b02d6107ebf75211a0a91db75cb50
adrian <jorge.adrian.sau18@gmail.com>
2017-07-24 00:02:10 -0500

    Primer Commit

---
+++ src/main/java/com/doggy/DoggyApplication.java
@@ -0,0 +1,12 @@
+package com.doggy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoggyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoggyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/doggy/DoggyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.doggy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DoggyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
